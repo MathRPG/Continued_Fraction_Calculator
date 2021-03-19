d957bcc87f48f6c941b26a438714031db6a4807c
MathRPG <maugusilva.santos@gmail.com>
2021-03-19 16:22:35 -0300

    Refactored UI

--- gui/main.py
+++ gui/main.py
@@ -1,109 +1,113 @@
+import math
 import sys
 
 import matplotlib as mpl
-import matplotlib.pyplot as plt
-from PyQt5 import QtWidgets as qtw, QtGui
+import matplotlib.pyplot
+from PyQt5 import QtWidgets, QtGui
 from matplotlib.backends.backend_agg import FigureCanvasAgg
 
 from gui.simple_continued_fraction_calculator_ui import Ui_SimpleContinuedFractionCalculatorForm
-from test import Ui_main_window
 from simple_continuous_fraction import simple_continuous_fraction
 
-class SimpleContinuedFractionCalculatorWindow(qtw.QWidget, Ui_SimpleContinuedFractionCalculatorForm):
+
+class SimpleContinuedFractionCalculatorWindow(QtWidgets.QWidget, Ui_SimpleContinuedFractionCalculatorForm):
+    EVAL_SYMBOLS = {
+        # VARIABLES
+        'pi': math.pi,
+        'e': math.e,
+        'phi': (1 + math.sqrt(5)) / 2,
+
+        # FUNCTIONS
+        'sqrt': math.sqrt,
+        'sin': math.sin,
+        'cos': math.cos,
+    }
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.setupUi(self)
 
+        self.form_depth_spin_box.setMinimum(1)
+        self.form_depth_spin_box.setMaximum(40)
 
-    pass
+        self.form_submit_push_button.clicked.connect(self.submit_form)
 
-class MainWindowUI(Ui_main_window):
+        self.show()
 
-    def setupUi(self, window):
-        super(MainWindowUI, self).setupUi(window)
-        self.push_button.pressed.connect(self.on_button_click)
+    def submit_form(self):
+        expression = self.form_expression_line_edit.text()
+        depth = self.form_depth_spin_box.value()
 
-    def on_button_click(self):
-        expression = self.line_edit.text()
-        depth = self.spin_box.value()
+        try:
+            evaluation = eval(expression, self.EVAL_SYMBOLS)
+        except (SyntaxError, NameError):
+            self.form_expression_line_edit.setText('')
+            QtWidgets.QMessageBox.critical(self, 'Error', 'Invalid Expression')
+            return
 
-        evaluation = eval(expression)
+        latex = None
 
-        # DEBUG
-        print('Expression: ', expression)
-        print('Depth : ', depth)
-        print('Eval: ', evaluation)
+        try:
+            latex = self.get_cf_latex_from_value(evaluation, depth)
+        except Exception as e:
+            self.form_depth_spin_box.setValue(15)
+            QtWidgets.QMessageBox.critical(self, 'Error', 'Calculation Failed')
+            print(e)
+            return
 
-        frac_latex = '$' + simple_continuous_fraction(evaluation, depth) + '$'
-        print(frac_latex)
+        pixmap = None
 
-        self.image.setPixmap(mathTex_to_QPixmap(frac_latex, 14))
-        self.image.show()
+        try:
+            pixmap = self.get_pixmap_from_latex(latex, font_size=12)
+        except Exception as e:
+            QtWidgets.QMessageBox.critical(self, 'Error', 'Rendering Failed')
+            print(e)
+            return
 
+        self.result_render_label.setPixmap(pixmap)
+        self.result_render_label.show()
 
-# Source: https://stackoverflow.com/questions/32035251/displaying-latex-in-pyqt-pyside-qtablewidget
-def mathTex_to_QPixmap(mathTex, font_size):
-    # ---- set up a mpl figure instance ----
+    @staticmethod
+    def get_cf_latex_from_value(value, depth):
+        return f'${simple_continuous_fraction(value, depth)}$'
 
-    fig = mpl.pyplot.Figure()
-    fig.patch.set_facecolor('none')
-    fig.set_canvas(FigureCanvasAgg(fig))
-    renderer = fig.canvas.get_renderer()
+    @staticmethod
+    def get_pixmap_from_latex(latex, font_size=20):
+        fig = mpl.pyplot.figure()
+        fig.patch.set_facecolor('none')
+        fig.set_canvas(FigureCanvasAgg(fig))
+        renderer = fig.canvas.get_renderer()
 
-    # ---- plot the mathTex expression ----
+        ax = fig.add_axes([0, 0, 1, 1])
+        ax.axis('off')
+        # ax.patch.set_facecolor('none')
+        t = ax.text(0, 0, latex, ha='left', va='bottom', fontsize=font_size)
 
-    ax = fig.add_axes([0, 0, 1, 1])
-    ax.axis('off')
-    ax.patch.set_facecolor('none')
-    t = ax.text(0, 0, mathTex, ha='left', va='bottom', fontsize=font_size)
+        fig_width, fig_height = fig.get_size_inches()
+        fig_bbox = fig.get_window_extent(renderer)
+        text_bbox = t.get_window_extent(renderer)
 
-    # ---- fit figure size to text artist ----
+        tight_fig_width = text_bbox.width * fig_width / fig_bbox.width
+        tight_fig_height = text_bbox.height * fig_height / fig_bbox.height
 
-    fwidth, fheight = fig.get_size_inches()
-    fig_bbox = fig.get_window_extent(renderer)
+        fig.set_size_inches(tight_fig_width, tight_fig_height)
 
-    text_bbox = t.get_window_extent(renderer)
+        buf, size = fig.canvas.print_to_buffer()
 
-    tight_fwidth = text_bbox.width * fwidth / fig_bbox.width
-    tight_fheight = text_bbox.height * fheight / fig_bbox.height
+        q_image = QtGui.QImage.rgbSwapped(QtGui.QImage(buf, size[0], size[1], QtGui.QImage.Format_ARGB32))
 
-    fig.set_size_inches(tight_fwidth, tight_fheight)
+        q_pixmap = QtGui.QPixmap(q_image)
 
-    # ---- convert mpl figure to QPixmap ----
-
-    buf, size = fig.canvas.print_to_buffer()
-    qimage = QtGui.QImage.rgbSwapped(QtGui.QImage(buf, size[0], size[1],
-                                                  QtGui.QImage.Format_ARGB32))
-    qpixmap = QtGui.QPixmap(qimage)
-
-    return qpixmap
+        return q_pixmap
 
 
 if __name__ == '__main__':
 
-    app = qtw.QApplication([])
+    sys.setrecursionlimit(10000)
+
+    app = QtWidgets.QApplication(sys.argv)
 
     widget = SimpleContinuedFractionCalculatorWindow()
-    widget.show()
+    # widget.show()
 
     sys.exit(app.exec_())
-
-
-    # print('hey')
-    # app = qtw.QApplication(sys.argv)
-    # print('hey')
-    #
-    # main_window = qtw.QMainWindow()
-    # print('hey')
-    #
-    # ui = MainWindowUI()
-    # print('hey')
-    #
-    # ui.setupUi(main_window)
-    # print('hey')
-    #
-    # main_window.show()
-    # print('hey')
-    #
-    # sys.exit(app.exec_())
