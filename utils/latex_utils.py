1fbffd42513f35e3ed2430e276dad6b084ee474b
MathRPG <maugusilva.santos@gmail.com>
2021-03-25 21:07:30 -0300

    Refactored and cleanup up

--- utils/latex_utils.py
+++ utils/latex_utils.py
@@ -1,7 +1,6 @@
 import sys
-from io import BytesIO
+import io
 
-import matplotlib as mpl
 import matplotlib.pyplot as plt
 from PyQt5 import QtGui as QtG
 from PyQt5.QtSvg import QSvgWidget
@@ -11,9 +10,7 @@ from matplotlib.backends.backend_agg import FigureCanvasAgg
 
 # Source: https://stackoverflow.com/questions/32035251/displaying-latex-in-pyqt-pyside-qtablewidget
 def latex_to_pixmap(latex, font_size=20):
-    mpl.pyplot.close('all')  # TODO: This is dumb
-
-    fig = mpl.pyplot.figure()
+    fig = plt.figure()
     fig.patch.set_facecolor('none')
     fig.set_canvas(FigureCanvasAgg(fig))
     renderer = fig.canvas.get_renderer()
@@ -32,6 +29,7 @@ def latex_to_pixmap(latex, font_size=20):
     fig.set_size_inches(tight_fig_width, tight_fig_height)
 
     buf, size = fig.canvas.print_to_buffer()
+    plt.close(fig)
 
     q_image = QtG.QImage.rgbSwapped(QtG.QImage(buf, size[0], size[1], QtG.QImage.Format_ARGB32))
 
@@ -40,62 +38,12 @@ def latex_to_pixmap(latex, font_size=20):
     return q_pixmap
 
 
-# Alternative latex rendering (I think SVG):
-# https://gist.github.com/gmarull/dcc8218385014559c1ca46047457c364
-# import sys
-# from PyQt5.QtWidgets import QApplication
-# from PyQt5.QtSvg import QSvgWidget
-#
-# from io import BytesIO
-# import matplotlib.pyplot as plt
-#
-#
-# # matplotlib: force computer modern font set
-# plt.rc('mathtext', fontset='cm')
-#
-#
-# def tex2svg(formula, fontsize=12, dpi=300):
-#     """Render TeX formula to SVG.
-#     Args:
-#         formula (str): TeX formula.
-#         fontsize (int, optional): Font size.
-#         dpi (int, optional): DPI.
-#     Returns:
-#         str: SVG render.
-#     """
-#
-#     fig = plt.figure(figsize=(0.01, 0.01))
-#     fig.text(0, 0, r'${}$'.format(formula), fontsize=fontsize)
-#
-#     output = BytesIO()
-#     fig.savefig(output, dpi=dpi, transparent=True, format='svg',
-#                 bbox_inches='tight', pad_inches=0.0, frameon=False)
-#     plt.close(fig)
-#
-#     output.seek(0)
-#     return output.read()
-#
-#
-# def main():
-#     FORMULA = r'\int_{-\infty}^\infty e^{-x^2}\,dx = \sqrt{\pi}'
-#
-#     app = QApplication(sys.argv)
-#
-#     svg = QSvgWidget()
-#     svg.load(tex2svg(FORMULA))
-#     svg.show()
-#
-#     sys.exit(app.exec_())
-#
-#
-# if __name__ == '__main__':
-#     main()
-
+# Source: https://gist.github.com/gmarull/dcc8218385014559c1ca46047457c364
 def latex_to_svg(latex, font_size=12, dpi=300):
     fig = plt.figure(figsize=(0.01, 0.01))
     fig.text(0, 0, latex, fontsize=font_size)
 
-    output_file = BytesIO()
+    output_file = io.BytesIO()
 
     fig.savefig(output_file, dpi=dpi, transparent=True, format='svg', bbox_inches='tight', pad_inches=0.0)
     plt.close(fig)
@@ -104,7 +52,7 @@ def latex_to_svg(latex, font_size=12, dpi=300):
     return output_file.read()
 
 
-if __name__ == '__main__':
+def __example_usage_latex_to_svg():
     plt.rc('mathtext', fontset='cm')
     latex = r'$1 + \dfrac{1}{1 + \dfrac{1}{3}}$'
     app = QApplication(sys.argv)
@@ -112,3 +60,7 @@ if __name__ == '__main__':
     svg.load(latex_to_svg(latex))
     svg.show()
     sys.exit(app.exec_())
+
+
+if __name__ == '__main__':
+    __example_usage_latex_to_svg()
